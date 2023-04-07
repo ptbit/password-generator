7155e62a289bbd23205666cc67ccf6ffc576ecb3
foxstudent103376 <yashopua@gmail.com>
2023-04-07 22:46:26 +0300

    base function

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 let body = document.querySelector("body");
+let generatedPassword = document.getElementById("GENERATED-PASSWORD");
+let passwordLength = document.getElementById("password-length");
+let passwordRange = document.getElementById("password-range");
 
-console.log("hi");
+
+
+
+passwordLength.addEventListener('click', clickHandler)
+passwordRange.addEventListener('click', clickHandler)
+
+function clickHandler (e) {
+  passwordLength.value = e.target.value
+  passwordRange.value = e.target.value
+
+  // generatePassword(e.target.value)
+  generatedPassword.value = generatePassword(e.target.value)
+}
+
+function generatePassword(passwordLength) {
+  let generatedPasswordArr = []
+  const allSymbols = LOWERCASE.concat(UPPERCASE.concat(NUMBERS.concat(SYMBOLS)));
+  for (let i = 0; i < passwordLength; i++) {
+    generatedPasswordArr.push(allSymbols[Math.floor(Math.random() * allSymbols.length)])
+  }
+  let generatedPasswordStr = generatedPasswordArr.join('')
+  return generatedPasswordStr
+}
